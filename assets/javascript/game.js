b85455b09e6a480f5648dc47104b62a9696ee6c4
stevenhorkey <stevenhorkey@gmail.com>
2018-02-15 19:04:48 -0700

    fixed text issues so that they show up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,26 +6,26 @@ var game = {
         balrog : {
             name: 'balrog',
             health: 150,
-            attack: 15,
+            attack: 16,
             imgURL: "http://eskipaper.com/images/scary-monster-wallpaper-1.jpg",
         },
         smeagol : {
             name: 'smeagol',
             health: 130,
-            attack: 15,
+            attack: 14,
             imgURL: "https://cdna.artstation.com/p/assets/images/images/004/532/170/large/eduardo-ruiz-urrejola-gollum-pose09.jpg?1484348343",  
         },
         gandalf : {
             name: 'gandalf',
             health: 160,
-            attack: 15,
+            attack: 18,
             imgURL: "https://vignette.wikia.nocookie.net/lotr/images/8/8d/Gandalf-2.jpg/revision/latest?cb=20130209172436",
             // createDiv: initCharacter()
         },
         sauron : {
             name: 'sauron',
             health: 180,
-            attack: 15,
+            attack: 20,
             imgURL: "https://wordsonfilmsdotcom.files.wordpress.com/2014/06/sauron2.png?w=600&h=330",
         }
     },
@@ -55,13 +55,14 @@ var game = {
             yourOpponent : "";
             charactersLeft = 4;
             $('.char-div').remove();
-            this.initCharacter(game.characters.gandalf.name, game.characters.gandalf.health,game.characters.gandalf.attack)
-            this.initCharacter(game.characters.smeagol.name, game.characters.smeagol.health,game.characters.smeagol.attack)
-            this.initCharacter(game.characters.balrog.name, game.characters.balrog.health,game.characters.balrog.attack)
-            this.initCharacter(game.characters.sauron.name, game.characters.sauron.health,game.characters.sauron.attack)
+            this.initCharacter(game.characters.gandalf.name, game.characters.gandalf.health,game.characters.gandalf.attack);
+            this.initCharacter(game.characters.smeagol.name, game.characters.smeagol.health,game.characters.smeagol.attack);
+            this.initCharacter(game.characters.balrog.name, game.characters.balrog.health,game.characters.balrog.attack);
+            this.initCharacter(game.characters.sauron.name, game.characters.sauron.health,game.characters.sauron.attack);
             game.functions.pickCharacter();
             $('.your-character').removeClass('col-xs-offset-3');
-            $('.storyline').show()
+            $('.titles').show();
+            $('.storyline').show();
             $('.character-choose').show();
             $('.your-character').hide();
             $('.fight-button').hide();
@@ -100,6 +101,8 @@ var game = {
         },
         pickEnemyToFight : function() {
             $('.enemy').on("click",function(event){
+                $('.win-text').show();                
+                $('.enemies').show();                
                 $('.opponent-death-note').hide()
                 $(this).addClass('yourOpponent');
                 yourOpponent = $(this);
@@ -127,12 +130,12 @@ var game = {
                 console.log(`Your Opponent's Health: ${opponentHealth}`);
                 if (opponentHealth <= 0 && yourHealth <= 0){
                     yourCharacter.remove();
-                    yourOpponent.remove()                             
+                    yourOpponent.remove();                             
                     $('.button-text').text("Restart!");
                     $('.fight-button').off("click");
-                    $('.titles').hide()
-                    $('.result-note').text("Drats! You have all died. Click the sheild to restart and redeem yourself...")
+                    $('.titles').hide();
                     $('.result-note').show();
+                    $('.result-note').text("Drats! You have all died. Click the sheild to restart and redeem yourself...");
                     $('.fight-button').on("click",function(event){
                         game.functions.initGame();
                     });                
